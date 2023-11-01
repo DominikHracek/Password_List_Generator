d1604b7635707d679d21f86bee034de5bf700c90
DominikHracek <domca.hracek+github@protonmail.com>
2023-11-01 10:44:48 +0100

    Extended help page

--- help.cpp
+++ help.cpp
@@ -10,8 +10,8 @@ void Help::show_general_help() {
   std::cout << '\n';
   std::cout << "Options:" << '\n';
   std::cout << '\t' << "-h" << " " << "|" << " " << "--help" << '\t' << "Shows this page" << '\n';
-  std::cout << '\t' << "-n" << " " << "|" << " " << "--min" << '\t' << "Minimal number of characters" << '\n';
-  std::cout << '\t' << "-x" << " " << "|" << " " << "--max" << '\t' << "Maximal number of characters" << '\n';
+  std::cout << '\t' << "-n" << " " << "|" << " " << "--min" << '\t' << "Minimal number of characters (default: 0)" << '\n';
+  std::cout << '\t' << "-x" << " " << "|" << " " << "--max" << '\t' << "Maximal number of characters (default: 12)" << '\n';
   std::cout << '\t' << "-r" << " " << "|" << " " << "--char" << '\t' << "What characters to use between words TODO <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << '\n';
   std::cout << '\t' << "-c" << " " << "|" << " " << "--case" << '\t' << "Upper/Lowercase settings" << '\n';
   std::cout << '\t' << "-f" << " " << "|" << " " << "--file" << '\t' << "Load words from a file" << '\n';
@@ -26,7 +26,7 @@ void Help::show_minimal_character_length(){
   std::cout << '\n';
   std::cout << "Example:" << '\n';
   std::cout << '\t' << "./kplg -n 8" << '\n';
-  std::cout << '\t' << "./kplg --min 8";
+  std::cout << '\t' << "./kplg --min 8" << '\n';
 }
 
 void Help::show_maximal_character_length(){
@@ -38,20 +38,20 @@ void Help::show_maximal_character_length(){
   std::cout << '\n';
   std::cout << "Example:" << '\n';
   std::cout << '\t' << "./kplg -m 16" << '\n';
-  std::cout << '\t' << "./kplg --max 16";
+  std::cout << '\t' << "./kplg --max 16" << '\n';
 }
 
 void Help::show_characters(){
   std::cout << "Usage:" << '\n';
-  std::cout << '\t' << "./kplg -r" << '\n';
-  std::cout << '\t' << "./kplg --char" << '\n';
+  std::cout << '\t' << "./kplg -r <(punctuation_file,)line_in_file>" << '\n';
+  std::cout << '\t' << "./kplg --char <(punctuation_file,)line_in_file>" << '\n';
   std::cout << '\n';
-  std::cout << "Decides what characters are going to be used between words (word-word, word_word, wordword) to increase the probability of getting the right password" << '\n';
-  // std::cout << "Characters are chosen from characters.kplg (Khakis's Password List Generator)";
+  std::cout << "Decides what punctuation (word-word, word_word, wordword) to increase the probability of getting the right password" << '\n';
   std::cout << '\n';
+  std::cout << "If not specified otherwise, characters.kplg is used as file for punctuation.";
   std::cout << "Example:" << '\n';
-  std::cout << '\t' << "./kplg -r TO-DO <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << '\n';
-  std::cout << '\t' << "./kplg --char TO-DO <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<";
+  std::cout << '\t' << "./kplg -r 13 (line 13 in file characters.kplg)" << '\n';
+  std::cout << '\t' << "./kplg --char my_characters.txt,15 (line 15 in file my_characters.txt)" << '\n';
 }
 
 void Help::show_case(){
@@ -60,6 +60,7 @@ void Help::show_case(){
   std::cout << '\t' << "./kplg --case" << '\n';
   std::cout << '\n';
   std::cout << "Decides, how much are letters switched from upper to lower case (0-n)" << '\n';
+  std::cout << '\t' << "0 - Leave as it is" << '\n';
   std::cout << '\t' << "1  - Create all combinations of switching the first letter of every word (word word, word Word, Word word, Word Word)";
   std::cout << '\t' << "2  - Same as 1, but with first, the middle one (both middle ones, if even amount of characters in word) and last)";
   std::cout << '\t' << "3a - With odd indexed characters (LiKe ThIs)" << '\n';
@@ -68,7 +69,7 @@ void Help::show_case(){
   std::cout << '\n';
   std::cout << "Example:" << '\n';
   std::cout << '\t' << "./kplg -c 1" << '\n';
-  std::cout << '\t' << "./kplg --case 1";
+  std::cout << '\t' << "./kplg --case 1" << '\n';
 }
 
 void Help::show_file(){
@@ -80,5 +81,5 @@ void Help::show_file(){
   std::cout << '\n';
   std::cout << "Example:" << '\n';
   std::cout << '\t' << "./kplg -f words.txt" << '\n';
-  std::cout << '\t' << "./kplg --file words";
+  std::cout << '\t' << "./kplg --file words" << '\n';
 }
