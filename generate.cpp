36e5b8c2e7a246ad2c8ff0bbe618dc9776a53933
DominikHracek <domca.hracek+github@protonmail.com>
2023-11-01 10:44:00 +0100

    Generate get_arguments() finished

--- generate.cpp
+++ generate.cpp
@@ -3,22 +3,90 @@
 
 #include "generate.h"
 
-int minimal_combination_length;
-int maximal_combination_length;
-int character_set; // int because of indentification by line
-int letter_case;
+Generate::Generate() {
+    minimal_combination_length = 0;
+    maximal_combination_length = 12;
+    character_set_file = "characters.kplg";
+    character_set_line = 1;
+    letter_case = 0;
+    input_file = "";
+    output_file = "";
+}
 
-std::string arguments[5][2] = {{"-n","--min"},
-                               {"-x","--max"},
-                               {"-r", "--char"},
-                               {"-c", "--case"},
-                               {"-f", "--file"}};
+void Generate::get_arguments(int argc, char *argv[]) {
+    for (int i = 1; i < argc; i += 2) {
+        std::string arg = argv[i];
+        std::string possible_value;
+        if (i + 1 < argc){
+          possible_value = argv[i + 1];
+        }
 
-void Generate::get_arguments(int argc, char *argv[]){
-  for (int i = 1; i < argc; i + 2){
-    if (strcmp(argv[i], "-n") == 0){
-      
+        if (arg == "-n" || arg == "--min") {
+            if (i + 1 < argc && possible_value.find("-") == -1) {
+                minimal_combination_length = std::stoi(argv[i + 1]);
+            } else {
+              std::cout << "Error: Missing value for -n/--min argument." << '\n';
+              break;
+            }
+        } else if (arg == "-x" || arg == "--max") {
+            if (i + 1 < argc && possible_value.find("-") == -1) {
+                maximal_combination_length = std::stoi(argv[i + 1]);
+            } else {
+              std::cout << "Error: Missing value for -x/--max argument." << '\n';
+              break;
+            }
+        } else if (arg == "-r" || arg == "--char") {
+            if (i + 1 < argc && possible_value.find("-") == -1) {
+              std::string character_file_checker = argv[i + 1];
+              if (character_file_checker.find(",") != -1){
+                character_set_file = character_file_checker.substr(0,character_file_checker.find(","));
+                character_set_line = std::stoi(character_file_checker.substr(character_file_checker.find(",") + 1));
+              } else {
+                character_set_line = std::stoi(argv[i + 1]);
+              }
+            } else {
+              std::cout << "Error: Missing value for -r/--char argument." << '\n';
+              break;
+            }
+        } else if (arg == "-c" || arg == "--case") {
+            if (i + 1 < argc && possible_value.find("-") == -1) {
+                letter_case = std::stoi(argv[i + 1]);
+            } else {
+              std::cout << "Error: Missing value for -c/--case argument." << '\n';
+              break;
+            }
+        } else if (arg == "-f" || arg == "--file") {
+            if (i + 1 < argc && possible_value.find("-") == -1) {
+                input_file = argv[i + 1];
+            } else {
+              std::cout << "Error: Missing value for -f/--file argument." << '\n';
+              break;
+            }
+        } else {
+          std::cout << "Error: Unknown argument " << arg << '\n';
+            break;
+        }
     }
+}
+
+void Generate::start_ui() {
+  std::cout << ">>>>>>>>>>>>>>>>>>>>>Khakis's Password List Generator<<<<<<<<<<<<<<<<<<<<<" << '\n';
+  std::cout << '\n';
+  std::cout << "Minimal combination length: " << minimal_combination_length << '\n';
+  std::cout << "Maximal combination length: " << maximal_combination_length << '\n';
+  std::cout << "Case-sensitivity level: " << letter_case << "/" << 4 << '\n';
+  std::cout << "Character set:" << '\n';
+  std::cout << '\t' << "Name: " << character_set_file << '\n';
+  if (character_set_line < 1){
+    character_set_line = 1;
+    std::cout << '\t' << "Line: " << character_set_line << '\n';
+    std::cout << '\t' << "Each file's first line start at index 1 -> index has been set to 1" << '\n';
+    std::cout << '\n';
+  } else {
+    std::cout << '\t' << "Line: " << character_set_line << '\n';
   }
 }
-void Generate::generate_combinations(){}
+
+void Generate::generate_combinations() {
+  
+}
--- generate.h
+++ generate.h
@@ -1,6 +1,23 @@
-class Generate{
+#ifndef GENERATE_H
+#define GENERATE_H
+
+#include <iostream>
+
+class Generate {
   public:
-    int find_in_array(char *array[], int length, int to_find);
+    Generate();
     void get_arguments(int argc, char *argv[]);
     void generate_combinations();
+    void start_ui();
+
+  private:
+    int minimal_combination_length;
+    int maximal_combination_length;
+    std::string character_set_file;
+    int character_set_line;
+    int letter_case;
+    std::string input_file;
+    std::string output_file;
 };
+
+#endif // GENERATE_H
