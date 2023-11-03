ed9bb94e74b1a46eb53a14e27bec104b5d7bfc64
DominikHracek <domca.hracek+github@protonmail.com>
2023-11-03 19:27:27 +0100

    Working with files

--- generate.cpp
+++ generate.cpp
@@ -1,5 +1,8 @@
+#include <chrono>
+#include <cmath>
 #include <cstring>
 #include <iostream>
+#include <thread>
 
 #include "generate.h"
 
@@ -9,8 +12,9 @@ Generate::Generate() {
     character_set_file = "characters.kplg";
     character_set_line = 1;
     letter_case = 0;
-    input_file = "";
-    output_file = "";
+    input_file_name = "";
+    output_file_name = "generated_passwords.txt";
+    words = {};
 }
 
 void Generate::get_arguments(int argc, char *argv[]) {
@@ -56,12 +60,12 @@ void Generate::get_arguments(int argc, char *argv[]) {
               break;
             }
         } else if (arg == "-f" || arg == "--file") {
-            if (i + 1 < argc && possible_value.find("-") == -1) {
-                input_file = argv[i + 1];
-            } else {
-              std::cout << "Error: Missing value for -f/--file argument." << '\n';
-              break;
-            }
+          if (i + 1 < argc && possible_value.find("-") == -1) {
+            input_file_name = argv[i + 1];
+          } else {
+            std::cout << "Error: Missing value for -f/--file argument." << '\n';
+            break;
+          }
         } else {
           std::cout << "Error: Unknown argument " << arg << '\n';
             break;
@@ -72,21 +76,70 @@ void Generate::get_arguments(int argc, char *argv[]) {
 void Generate::start_ui() {
   std::cout << ">>>>>>>>>>>>>>>>>>>>>Khakis's Password List Generator<<<<<<<<<<<<<<<<<<<<<" << '\n';
   std::cout << '\n';
+  std::this_thread::sleep_for(std::chrono::milliseconds(25));
   std::cout << "Minimal combination length: " << minimal_combination_length << '\n';
+  std::this_thread::sleep_for(std::chrono::milliseconds(25));
   std::cout << "Maximal combination length: " << maximal_combination_length << '\n';
+  std::this_thread::sleep_for(std::chrono::milliseconds(25));
   std::cout << "Case-sensitivity level: " << letter_case << "/" << 4 << '\n';
+  std::this_thread::sleep_for(std::chrono::milliseconds(25));
   std::cout << "Character set:" << '\n';
+  std::this_thread::sleep_for(std::chrono::milliseconds(25));
   std::cout << '\t' << "Name: " << character_set_file << '\n';
+  std::this_thread::sleep_for(std::chrono::milliseconds(25));
   if (character_set_line < 1){
     character_set_line = 1;
     std::cout << '\t' << "Line: " << character_set_line << '\n';
+    std::this_thread::sleep_for(std::chrono::milliseconds(25));
     std::cout << '\t' << "Each file's first line start at index 1 -> index has been set to 1" << '\n';
+    std::this_thread::sleep_for(std::chrono::milliseconds(25));
     std::cout << '\n';
   } else {
     std::cout << '\t' << "Line: " << character_set_line << '\n';
+    std::this_thread::sleep_for(std::chrono::milliseconds(25));
+  }
+  std::cout << "File with words: " << input_file_name << '\n';
+  std::cout << "File with generated passwords: " << output_file_name << '\n';
+}
+
+void Generate::read_file(std::string input_file_name) {
+  input_file.open(input_file_name);
+  if (input_file.is_open()){
+    while(input_file.good()){
+      std::string line_in_file;
+      input_file >> line_in_file;
+      words.push_back(line_in_file);
+    }
+    int max_word_length = 0;
+    for (std::string word : words) {
+      if (word.length() > max_word_length) {
+        max_word_length = word.length();
+      }
+    }
+
+    for (int i = 0; i < words.size(); i++) {
+      std::cout << '\r';
+      for (int j = 0; j < max_word_length; j++) {
+        std::cout << ' '; // Clear the line
+      }
+      std::cout << '\r' << words[i];
+      std::cout.flush(); // Flush the output to make it visible
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    std::cout << std::endl;
   }
 }
 
+void Generate::ask_for_patterns() {}
+
 void Generate::generate_combinations() {
-  
+  read_file(input_file_name);
+  // number_of_words^number_of_places_for_words     * number_of_punctuation^number_of_places_for_punctuation
+  // (word1 word2; word2 word3, word3 word1 -> 3^2) * ( ,-,_,/)4^2 (-/,-_, etc.)
+  //for (int i = 1; i <=pow(words.size()); i++){}
+  /*
+  * TO-DO
+  * If word is longer, than the expected output length, shorten it from the beginning, end, the middle and evenly both words
+  */
 }
--- generate.h
+++ generate.h
@@ -1,14 +1,20 @@
 #ifndef GENERATE_H
 #define GENERATE_H
 
+#include <fstream>
 #include <iostream>
+#include <string>
+#include <vector>
 
 class Generate {
   public:
     Generate();
     void get_arguments(int argc, char *argv[]);
+    void ask_for_patterns();
     void generate_combinations();
     void start_ui();
+    void start_friendly_ui();
+    void read_file(std::string input_file_name);
 
   private:
     int minimal_combination_length;
@@ -16,8 +22,11 @@ class Generate {
     std::string character_set_file;
     int character_set_line;
     int letter_case;
-    std::string input_file;
-    std::string output_file;
+    std::ifstream input_file;
+    std::string input_file_name;
+    std::ifstream output_file;
+    std::string output_file_name;
+    std::vector<std::string> words;
 };
 
 #endif // GENERATE_H
