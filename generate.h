c6b7368c98fdee058941561052a83f8b72379ba0
BuildTools <unconfigured@null.spigotmc.org>
2023-12-01 06:10:33 +0100

    Moving functions to another file

--- generate.h
+++ generate.h
@@ -1,51 +1,15 @@
 #ifndef GENERATE_H
 #define GENERATE_H
 
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <vector>
+class Generate{
+    public:
+        Generate();
+        void get_info();
+        void generate_combinations();
 
-class Generate {
-  public:
-    Generate();
-    void get_arguments(int argc, char *argv[]);
-    void is_everything_ok();
-    void start_ui();
-    void start_friendly_ui();
-    void get_separators(std::string separators_file_name, int line);
-    void get_separators(int line);
-    void get_words(std::string input_file_name);
-    void get_words();
-    int count_occurrences(std::string string, char character);
-    void ask_for_patterns();
-    void generate_combinations();
-
-  private:
-    int minimal_combination_length;
-    int maximal_combination_length;
-
-    bool use_same_word_multiple_times_in_one;
-
-    std::ifstream separators_file;
-    std::string separators_file_name;
-    int separators_line;
-    std::vector<std::string> separators;
-    std::string separator;
-
-    int letter_case;
-
-    std::string input_file_name;
-    std::ifstream input_file;
-
-    std::vector<std::string> words;
-    std::vector<char> patterns_of_words;
-
-    std::vector<std::string> word_combinations;
-    std::vector<std::vector<std::string>> combinations;
-
-    std::string output_file_name;
-    std::ifstream output_file;
+    private:
+        bool use_same_word_multiple_times_in_one;
+        int multiplier;
 };
 
-#endif // GENERATE_H
+#endif
