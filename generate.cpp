254ae5e47e6b259ff81ddd0fa4ac24d866556312
BuildTools <unconfigured@null.spigotmc.org>
2023-12-01 20:47:51 +0100

    Fixing errors

--- generate.cpp
+++ generate.cpp
@@ -1,37 +1,48 @@
 #include <iostream>
+#include <vector>
 
 #include "generate.h"
 
 Generate::Generate() {
 	use_same_word_multiple_times_in_one = false;
+	total_combinations = 0;
 }
 
-void Generate::get_info() {}
+void Generate::get_info(int minimal_combination_length,
+                        int maximal_combination_length,
+                        int letter_case,
+                        std::string separator,
+                        std::vector<std::vector<std::string>> combinations) {
 
-void Generate::generate_combinations() {
-	int total_words;
+	this->minimal_combination_length = minimal_combination_length;
+	this->maximal_combination_length = maximal_combination_length;
+	this->letter_case = letter_case;
+	this->separator = separator;
+	this->combinations = combinations;
+}
+
+void Generate::calculate_number_of_combinations() {
+	int total_words = 0;
 	for (auto combination : combinations){
 		total_words += combination.size();
 	}
 	for (int i = minimal_combination_length; i <=maximal_combination_length; i++){
-		int total_combinations = total_words;
+		total_combinations = total_words;
 		if (!use_same_word_multiple_times_in_one){
 			int multiplier = 1;
 			for (int i = total_words - 1; i > 0; i--){
-				std::cout << multiplier << '\n';
 				multiplier *= i;
 				total_combinations += total_words * multiplier;
 			}
-			std::cout << multiplier << '\n';
 		} else {
-		int multiplier = 1;
-		for (int i = total_words - 1; i > 0; i--){
-		std::cout << multiplier << '\n';
-		multiplier *= 5;
-		total_combinations += total_words * multiplier;
-		}
-		std::cout << multiplier << '\n';
+			int multiplier = 1;
+			for (int i = total_words - 1; i > 0; i--){
+				multiplier *= 5;
+				total_combinations += total_words * multiplier;
+			}
 		}
 	}
-	exit(0);
+	Generate::generate_combinations();
 }
+
+void Generate::generate_combinations() {}
--- generate.h
+++ generate.h
@@ -1,14 +1,29 @@
 #ifndef GENERATE_H
 #define GENERATE_H
 
+#include <iostream>
+#include <string>
+#include <vector>
 class Generate{
     public:
         Generate();
-        void get_info();
+        void get_info(int minimal_combination_length,
+                      int maximal_combination_length,
+                      int letter_case,
+                      std::string separator,
+                      std::vector<std::vector<std::string>> combinations);
+        void calculate_number_of_combinations();
         void generate_combinations();
 
     private:
+        int minimal_combination_length;
+        int maximal_combination_length;
+        int letter_case;
+        std::string separator;
+        std::vector<std::vector<std::string>> combinations;
+
         bool use_same_word_multiple_times_in_one;
+        int total_combinations;
         int multiplier;
 };
 
